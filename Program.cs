2c22f54e2b3cd59c1ac0cfe5c29bdc90f6fb97d7
Vladislav <katana0847@mail.ru>
2023-02-12 13:13:05 +0300

    test push

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+// Console.WriteLine("Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int quad = num  * num;
+// Console.WriteLine($"quad of {num} is {quad}");
+// Задача 2. 
+// задача 3.
+
+// Console.WriteLine("Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int current = num * (-1);
+
+// while (current <= num) 
+// { 
+//     Console.Write(num + " ");
+//     num--;
+// }    
+
+// Задача 4.
+
+Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = num % 10; 
+
+Console.WriteLine($"last number of {num} is {result}");
